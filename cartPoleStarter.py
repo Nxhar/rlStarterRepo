8d9dc50ffee420f0173e947dc44aa0dafc499af4
Nxhar <snsista04@gmail.com>
2024-12-20 14:02:49 +0530

    added repo files

---
+++ cartPoleStarter.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+from stable_baselines3 import PPO
+
+
+# Step 1 - Creating Environment and Resetting to begin
+
+
+# Step 2 - Creating and Training our Model
+
+
+# Step 3 - Check what our observation space, and action space looks like
+
+
+# Step 4 - Evaluate and Run Episodes (1 episode == 1 Game)
+
+
+# Step 5 - Plotting Results and Closing Environment
